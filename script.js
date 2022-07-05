4d73c382f34ed0ded28c8f3d7a35ae15f31364fa
Renan Fernandes <renzinestuods@gmail.com>
2022-07-05 14:26:44 -0300

    Adiciona a cor preta como a primeira cor da paleta de cores

---
+++ script.js
@@ -0,0 +1,4 @@
+const colorPalette = document.getElementById('color-palette');
+const colors = document.getElementsByClassName('color');
+const pixelBoard = document.getElementById('pixel-board');
+const pixels = document.getElementsByClassName('pixel');
