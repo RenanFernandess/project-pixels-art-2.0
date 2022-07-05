f34227e8f9082df48be82a23f700cf50ceab401b
Renan Fernandes <renzinestuods@gmail.com>
2022-07-05 19:42:36 -0300

    Faz com que as cores na paleta sejão jeradas de forma aleatoria, exeto a cor preta

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ const colorPalette = document.getElementById('color-palette');
 const colors = document.getElementsByClassName('color');
 const pixelBoard = document.getElementById('pixel-board');
 const pixels = document.getElementsByClassName('pixel');
+
+const colorGenerator = () => {
+  const r = Math.round(Math.random() * 250);
+  const g = Math.round(Math.random() * 250);
+  const b = Math.round(Math.random() * 250);
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+const colorAdd = () => [...colors].map(({ style }) => {
+  if (style.backgroundColor !== 'black') style.backgroundColor = colorGenerator();
+});
+
+// chama functions
+colorAdd();
