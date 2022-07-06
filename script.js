b88bd430d49cc2cb29f29c1fe4d33d6e58a4252a
Renan Fernandes <renzinestuods@gmail.com>
2022-07-06 11:14:54 -0300

    Adiciona um botão para limpar o conteudo

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const colorPalette = document.getElementById('color-palette');
 const colors = document.getElementsByClassName('color');
 const pixelBoard = document.getElementById('pixel-board');
 const pixels = document.getElementsByClassName('pixel');
+const buttonClear = document.getElementById('clear-board');
 
 const selected = () => document.querySelector('.selected');
 
@@ -26,11 +27,14 @@ const selectColor = ({ target: { classList } }) => {
 const colored = ({ target: { classList, style } }) => {
   if (classList.contains('pixel')) style
     .backgroundColor = selected().style.backgroundColor;
-}
+};
+
+const clearBoard = () => [...pixels].map(({ style }) => { style.backgroundColor = 'white'; });
 
 const events = () => {
   colorPalette.addEventListener('click', selectColor)
   pixelBoard.addEventListener('click', colored);
+  buttonClear.addEventListener('click', clearBoard);
 };
 
 // chama functions
