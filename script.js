c7f3db02d06e55b7d08e96bdb879f967c13c1684
Renan Fernandes <renzinestuods@gmail.com>
2022-07-06 10:54:43 -0300

    Agora é possivel prencher os pixels

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const colors = document.getElementsByClassName('color');
 const pixelBoard = document.getElementById('pixel-board');
 const pixels = document.getElementsByClassName('pixel');
 
+const selected = () => document.querySelector('.selected');
+
 const colorGenerator = () => {
   const r = Math.round(Math.random() * 250);
   const g = Math.round(Math.random() * 250);
@@ -10,9 +12,27 @@ const colorGenerator = () => {
   return `rgb(${r}, ${g}, ${b})`;
 };
 
-const colorAdd = () => [...colors].map(({ style }) => {
+const colorAdd = () => [...colors].forEach(({ style }) => {
   if (style.backgroundColor !== 'black') style.backgroundColor = colorGenerator();
 });
 
+const selectColor = ({ target: { classList } }) => {
+  if (classList.contains('color')) {
+    selected().classList.remove('selected');
+    classList.add('selected');
+  };
+};
+
+const colored = ({ target: { classList, style } }) => {
+  if (classList.contains('pixel')) style
+    .backgroundColor = selected().style.backgroundColor;
+}
+
+const events = () => {
+  colorPalette.addEventListener('click', selectColor)
+  pixelBoard.addEventListener('click', colored);
+};
+
 // chama functions
 colorAdd();
+events();
