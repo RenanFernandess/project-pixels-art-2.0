b94654ce61b07ddd4cc1022d596d3cc315e244df
Renan Fernandes <renzinestuods@gmail.com>
2022-08-20 10:38:59 -0300

    Refatora as funções calculateNextIndex e calculatePreviousIndex, para ser utilizada dentro de classes e poder reaproveitalas

--- src/helpers/Componente.js
+++ src/helpers/Componente.js
@@ -3,7 +3,7 @@ class Componente {
     this.state = {};
   }
 
-  getSavedState() { this.state = getItemSessionStorage('state') || {}; }
+  getSavedState() { this.state = getItemSessionStorage('state') || this.state; }
 
   setState(object, callback) {
     this.state = Object.assign(this.state, object);
@@ -11,6 +11,42 @@ class Componente {
     saveItemSessionStorage('state', this.state);
     callback();
     this.render();
+  }
+
+  // createListingState() {
+  //   this.state = {
+  //     number: 0,
+  //     lastIndex: 0,
+  //     firstIndex: 0,
+  //     numberOfBoard: 0,
+  //     pagesNumber: 0,
+  //     currentPage: 1,
+  //   };
+  // }
+
+  calculateNextIndex() {
+    const { currentPage, lastIndex, number, pageNumber, numberOfBoard } = this.state;
+    const nextPage = currentPage + 1;
+    let newlastIndex = (nextPage === pageNumber) ? numberOfBoard : lastIndex + number;
+    newlastIndex = (newlastIndex > numberOfBoard) ? numberOfBoard : newlastIndex;
+    const firstIndex = (newlastIndex - number);
+    this.setState({
+      currentPage: nextPage,
+      firstIndex: (firstIndex < 0) ? 0 : firstIndex,
+      lastIndex: newlastIndex,
+    });
+  }
+  
+  calculatePreviousIndex() {
+    const { currentPage, firstIndex, number } = this.state;
+    const previousPage = (firstIndex - number > 0) ? currentPage - 1 : 1;
+    console.log(previousPage);
+    const nextFirstIndex = (previousPage === 1) ? 0 : firstIndex - number;
+    this.setState({
+      currentPage: previousPage,
+      firstIndex: ((nextFirstIndex < 0) ? 0 : nextFirstIndex),
+      lastIndex: (previousPage === 1) ? number : firstIndex,
+    });
   }
 }
 
