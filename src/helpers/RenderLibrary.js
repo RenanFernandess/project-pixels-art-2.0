aa1343aace68084c6bb021c5119f8f9216d02d9d
Renan Fernandes <renzinestuods@gmail.com>
2022-08-20 11:00:40 -0300

    Remove a função numberOfBoardThatWillBeListe

--- src/helpers/RenderLibrary.js
+++ src/helpers/RenderLibrary.js
@@ -11,16 +11,6 @@ class RenderLibrary extends Componente {
     this.getSavedState();
   }
 
-  numberOfBoardThatWillBeListed(element, list) {
-    let number = (Math.floor(element.offsetWidth / 200) - 1);
-    const numberOfBoard = list.length;
-    number = (number <= 0) ? 1 : number;
-    this.setState({
-      number: (number > numberOfBoard) ? numberOfBoard : number,
-      numberOfBoard,
-      pagesNumber: (Math.round(numberOfBoard / number)),
-    });
-  }
   // renderList() {
   //   this.boardSavedList.forEach((board) => {
   //     boardsList.innerHTML += new CreatePreview(board);
