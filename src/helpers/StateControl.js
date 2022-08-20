c3147edf2b6847f8fa6de9665ef10f38496b985e
Renan Fernandes <renzinestuods@gmail.com>
2022-08-19 21:15:16 -0300

    Cria a função getSavedState que é responsavel por recuperar estado da section storage. Cria tambem a função setState que é responsavel por atualizar o state

--- src/helpers/StateControl.js
+++ src/helpers/StateControl.js
@@ -1,11 +1,15 @@
 class StateControl {
   constructor() {
-    this.state = getItemSessionStorage('state') || {};
+    this.state = {};
   }
 
-  createState(state) {
-    this.state = state;
+  getSavedState() { this.state = getItemSessionStorage('state') || {}; }
+
+  setState(object, callback) {
+    this.state = Object.assign(this.state, object);
+    console.log('state: ', this.state);
     saveItemSessionStorage('state', this.state);
+    callback();
   }
 }
 
