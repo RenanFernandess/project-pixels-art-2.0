b6bd6ae0d79c40aaffa3f97cd3a86365f43a0cbb
Renan Fernandes <renzinestuods@gmail.com>
2022-08-31 18:32:17 -0300

    Exporta as funções

--- src/helpers/storage.js
+++ src/helpers/storage.js
@@ -1,20 +1,11 @@
-function saveItem(name, item) {
+export default function saveItem(name, item) {
   localStorage.setItem(name, (JSON.stringify(item)));
 }
 
-const getSavedItem = (name) => JSON.parse(localStorage.getItem(name));
+export const getSavedItem = (name) => JSON.parse(localStorage.getItem(name));
 
-function saveItemSessionStorage(name, item) {
+export function saveItemSessionStorage(name, item) {
   sessionStorage.setItem(name, JSON.stringify(item));
 }
 
-const getItemSessionStorage = (name) => JSON.parse(sessionStorage.getItem(name));
-
-if (typeof module !== 'undefined') {
-  module.exports = {
-    saveItem,
-    getSavedItem,
-    saveItemSessionStorage,
-    getItemSessionStorage,
-  };
-}
+export const getItemSessionStorage = (name) => JSON.parse(sessionStorage.getItem(name));
