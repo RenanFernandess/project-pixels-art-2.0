22fabc24ff6d1dc189f881a2f6d994f96e72dee3
Renan Fernandes <renzinestuods@gmail.com>
2022-09-05 18:29:35 -0300

    Adiciona filtragem por nome e id atraves do input search

--- src/helpers/RenderLibrary.js
+++ src/helpers/RenderLibrary.js
@@ -5,6 +5,7 @@ import { globalState } from './GlobalState.js';
 const boardsList = document.getElementById('boards-list');
 const buttonNext = document.getElementById('next-list');
 const buttonPrevious = document.getElementById('previous-list');
+const searchBoardInput = document.getElementById('search-board');
 
 export default class RenderLibrary extends Componente {
   constructor() {
@@ -15,11 +16,12 @@ export default class RenderLibrary extends Componente {
     this.nextListOfBoard = this.nextListOfBoard.bind(this);
     this.previousListOfBoard = this.previousListOfBoard.bind(this);
     this.setUpdate = this.setUpdate.bind(this);
+    this.inputChange = this.inputChange.bind(this);
 
     this.state = {
       boardList: [],
-      currentLocation: 'library',
-      currentList: 'boardList',
+      list: [],
+      search: '',
       trash: [],
     };
     this.whenTheClassIsReady();
@@ -27,11 +29,11 @@ export default class RenderLibrary extends Componente {
 
   whenTheClassIsReady() {
     const state = globalState.getState(({ library }) => library);
-    const { currentList } = state;
-    console.log('uai', state[currentList]);
+    const { boardList } = state;
+    console.log('uai', boardList);
     this.setState({
       ...state,
-      ...this.numberOfBoardThatWillBeListed(boardsList, state[currentList]),
+      ...this.numberOfBoardThatWillBeListed(boardsList, boardList),
     });
     this.createListingState();
   }
@@ -39,7 +41,8 @@ export default class RenderLibrary extends Componente {
   setUpdate() {
     const state = globalState.getState(({ library }) => library);
     const { currentList } = state;
-    this.orUpdateTheList(boardsList, state[currentList], state);
+    const list = state[currentList];
+    this.orUpdateTheList(boardsList, list, { ...state, list });
   }
 
   nextListOfBoard() {
@@ -57,21 +60,28 @@ export default class RenderLibrary extends Componente {
   }
 
   resetList() {
-    const { boardList } = this.state;
-    this.checkIfChanged(boardsList, boardList);
+    const { list } = this.state;
+    this.checkIfChanged(boardsList, list);
+  }
+
+  inputChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
   }
 
   async render() {
-    const { firstIndex, lastIndex, currentList } = this.state;
-      if (this.state[currentList].length) {
+    const { firstIndex, lastIndex, list, currentList, search, currentLocation } = this.state;
+      if (list.length) {
         window.addEventListener('resize', this.resetList);
         buttonNext.addEventListener('click', this.nextListOfBoard);
         buttonPrevious.addEventListener('click', this.previousListOfBoard);
+        searchBoardInput.addEventListener('input', this.inputChange);
+        searchBoardInput.value = search;
         boardsList.innerHTML = '';
-        this.state[currentList].slice(firstIndex, lastIndex).forEach((board) => {
+        list.filter(({ name, id }) => name.includes(search) || search === id || !search)
+          .slice(firstIndex, lastIndex).forEach((board) => {
           const preview = new CreatePreview(board, currentList === 'trash');
           boardsList.appendChild(preview.renderPreview());
         });
-      } else boardsList.innerHTML = '<p>vaisa<p>';
+      } else boardsList.innerHTML = `<p>${currentLocation} está vaisa<p>`;
   }
 }
