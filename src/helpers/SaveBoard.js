9919aaa4e97edc9ae9823ffa4b0da00b4ef70f4f
Renan Fernandes <renzinestuods@gmail.com>
2022-09-13 20:39:01 -0300

    Altera a forma como o quadro é salvo

--- src/helpers/SaveBoard.js
+++ src/helpers/SaveBoard.js
@@ -5,29 +5,22 @@ import Componente from './Componente.js';
 const TRASH = 'trash';
 const BOARDSAVEDLIST = 'boardSavedList';
 export const LIBRARY = 'library';
-const BOARD = 'board';
+export const BOARD = 'board';
+const PIXELBOARD = 'pixelBoard';
 
-const inputBoardSize = document.getElementById('board-size');
-const inputBoardName = document.getElementById('board-name');
-const pixelBoard = document.getElementById('pixel-board');
+const pixelBoardElement = document.getElementById('pixel-board');
 const paragraphMessage = document.getElementById('error-mesage');
-const buttonSaveAs = document.getElementById('save-board-as');
-const buttonSave = document.getElementById('save-board');
 
 export default class SaveBoard extends Componente {
   constructor() {
     super();
     this.binds();
     this.state = {
-      author: '',
-      board: '',
-      boardNumber: 0,
-      favorited: false,
-      id: '',
-      date: {},
-      name: '',
-      size: pixelBoard.childElementCount,
-      nameRepeated: false,
+      currentBoard: {},
+      editing: false,
+      editBoardIndex: 0,
+      boardNameRepeated: false,
+      size: pixelBoardElement.childElementCount,
     };
     this.boardSavedList = getSavedItem(BOARDSAVEDLIST) || [];
     this.trash = getItemSessionStorage(TRASH) || [];
@@ -35,14 +28,10 @@ export default class SaveBoard extends Componente {
   }
 
   binds() {
-    this.inputChange = this.inputChange.bind(this);
-    this.saveBoard = this.saveBoard.bind(this);
     this.validateName = this.validateName.bind(this);
     this.generateId = this.generateId.bind(this);
-    this.updateBoardInfos = this.updateBoardInfos.bind(this);
     this.addNewBoard = this.addNewBoard.bind(this);
-    this.saveCurrentBoard = this.saveCurrentBoard.bind(this);
-    this.loadSavedBoard = this.loadSavedBoard.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
   }
 
   whenTheClassIsReady() {
@@ -51,75 +40,55 @@ export default class SaveBoard extends Componente {
       trash: this.trash,
     }, LIBRARY);
     this.render();
-    this.loadSavedBoard();
   }
 
-  loadSavedBoard() {
-    const savedBoard = getSavedItem(BOARD);
-    pixelBoard.innerHTML = savedBoard.board;
-    this.setState({ ...savedBoard });
-  }
-
-  saveCurrentBoard() {
-    this.setState({ board: pixelBoard.innerHTML, size: pixelBoard.childElementCount },
-      ({ author, board, boardNumber, date, favorited, id, name, size }) => {
-      saveItem(BOARD, { author, board, boardNumber, date, favorited, id, name, size });
+  setUpdate() {
+    const state = globalState.getState(({ pixelBoard }) => pixelBoard);
+    const { currentBoard, saveBoard, boardNameRepeated } = state;
+    this.setState({ currentBoard, boardNameRepeated }, () => {
+      if (saveBoard) {
+        globalState.pushState({ saveBoard: false }, PIXELBOARD);
+        this.saveBoard();
+      }
     });
-  }
-
-  inputChange({ target: { value, name } }) {
-    this.setState({
-        [name]: value,
-        nameRepeated: (name === 'name') && this.checksThatTheNameIsNotRepeated(value),
-      });
-  }
-
-  checksThatTheNameIsNotRepeated(boardName) {
-    const result = this.boardSavedList.some(({ name }) => name === boardName);
-    if (result) paragraphMessage.innerText = `'${boardName}' já está sendo usado!`;
-    else paragraphMessage.innerText = '';
-    return result;
   }
 
   saveBoard() {
     try {
-    this.validateName();
-    this.setState(this.updateBoardInfos, this.addNewBoard);
+      this.validateName();
+      this.addNewBoard();
     } catch (error) {
       paragraphMessage.innerText = error.message;
       console.log(error);
     }
   }
 
-  updateBoardInfos() {
-    const size = pixelBoard.childElementCount;
-    const board = pixelBoard.innerHTML;
-    const boardNumber = this.boardSavedList.length;
-    const id = this.generateId();
-    return { size, board, boardNumber, id };
-  }
-
-  addNewBoard({ author, board, boardNumber, date, favorited, id, name, size }) {
+  addNewBoard() {
+      const { currentBoard, currentBoard: { size, name } } = this.state;
+      const boardNumber = this.boardSavedList.length;
+      const id = this.generateId();
       const newList = [
         ...this.boardSavedList,
-        { author, board, boardNumber, date, favorited, id, name, size: `${size}/${size}` },
+        { ...currentBoard, boardNumber, id, size: `${size}/${size}` },
       ];
+      this.setState({
+        boardNameRepeated: newList.some(({ name: boardName }) => name === boardName),
+      });
       this.boardSavedList = newList;
       globalState.pushState({ boardList: newList }, LIBRARY);
       saveItem(BOARDSAVEDLIST, newList);
-      this.setState({ nameRepeated: newList.some(({ name: boardName }) => name === boardName) });
   }
 
   validateName() {
-    const { name, nameRepeated } = this.state;
-    if (nameRepeated) throw new Error(`'${name}' já está sendo usado!!`);
+    const { currentBoard: { name }, boardNameRepeated } = this.state;
+    if (boardNameRepeated) throw new Error(`'${name}' já está sendo usado!!`);
     if (name === '' || /^(\s+)$/g.test(name)) {
       throw new Error('Digite o nome do quadro para proceguir!');
     }
   }
 
   generateId() {
-    const { name, boardNumber, size } = this.state;
+    const { currentBoard: { name, boardNumber, size } } = this.state;
     const number = Math.round(Math.random() * 999);
     const string = name.replace(/\s+/g, '').substr(0, 4).toUpperCase();
     return `BOARD${number}${string}S${size}Z${boardNumber}`;
@@ -134,6 +103,15 @@ export default class SaveBoard extends Componente {
     }, LIBRARY);
     saveItem(BOARDSAVEDLIST, this.boardSavedList);
     saveItemSessionStorage(TRASH, this.trash);
+  }
+
+  async editBoard(boardId) {
+    let editBoardIndex;
+    const board = this.boardSavedList.find(({ id }, index) => {
+      editBoardIndex = index;
+      return id === boardId;
+    });
+    this.setState({ ...board, size: board.size[0], editing: true, editBoardIndex });
   }
 
   async restoreBoard(boardId) {
@@ -154,13 +132,7 @@ export default class SaveBoard extends Componente {
   }
 
   render() {
-    const { name, size } = this.state;
-    inputBoardName.addEventListener('input', this.inputChange);
-    inputBoardName.value = name;
-    inputBoardSize.addEventListener('input', this.inputChange);
-    inputBoardSize.value = size;
-    buttonSaveAs.addEventListener('click', this.saveBoard);
-    buttonSave.addEventListener('click', this.saveCurrentBoard);
+    return this.state;
   }
 }
 
