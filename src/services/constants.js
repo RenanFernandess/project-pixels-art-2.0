c05a9d4fc923c28f98482d47e77833524e2869db
Renan Fernandes <renzinestuods@gmail.com>
2022-09-14 15:13:22 -0300

    Cria o arquivo constants.js que contera todas as constatntes

---
+++ src/services/constants.js
@@ -0,0 +1,12 @@
+export const TRASH = 'trash';
+export const BOARDSAVEDLIST = 'boardSavedList';
+export const LIBRARY = 'library';
+export const BOARD = 'board';
+export const PIXELBOARD = 'pixelBoard';
+
+export const PIXEL_BOARD = document.getElementById('pixel-board');
+export const WARNING_MESSAGE_PARAGRAPH = document.getElementById('warning-message');
+export const INPUT_BOARD_SIZE = document.getElementById('board-size');
+export const INPUT_BOARD_NAME = document.getElementById('board-name');
+export const BUTTON_SAVE = document.getElementById('save-board');
+export const BUTTON_SAVE_AS = document.getElementById('save-board-as');
