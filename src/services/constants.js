feea19937ed9378b6d5dd43725613c191ab303c1
Renan Fernandes <renzinestuods@gmail.com>
2022-09-14 17:27:11 -0300

    adiciona a const RGB_COLOR_REGEXP

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,19 +1,21 @@
-export const TRASH = 'trash';
+export const BOARD = 'board';
 export const BOARDSAVEDLIST = 'boardSavedList';
 export const LIBRARY = 'library';
-export const BOARD = 'board';
 export const PIXELBOARD = 'pixelBoard';
+export const TRASH = 'trash';
 
-export const PIXEL_BOARD = document.getElementById('pixel-board');
-export const WARNING_MESSAGE_PARAGRAPH = document.getElementById('warning-message');
-export const INPUT_BOARD_SIZE = document.getElementById('board-size');
-export const INPUT_BOARD_NAME = document.getElementById('board-name');
+export const RGB_COLOR_REGEXP = /rgb\((\d+[,] \d+[,] \d+)\)/g;
+
+export const BUTTON_CLEAR = document.getElementById('clear-board');
 export const BUTTON_SAVE = document.getElementById('save-board');
 export const BUTTON_SAVE_AS = document.getElementById('save-board-as');
-export const PALETTE_CONTAINER = document.getElementById('palette-container');
-export const COLORS_DIV = document.getElementsByClassName('color');
-export const BUTTON_CLEAR = document.getElementById('clear-board');
-export const INPUT_COLOR_CONTAINER = document.getElementById('box-new-color');
-export const INPUT_COLOR = document.getElementById('new-color');
 export const BUTTON_SET_BOARD = document.getElementById('generate-board');
+export const COLORS_DIV = document.getElementsByClassName('color');
+export const INPUT_BOARD_NAME = document.getElementById('board-name');
+export const INPUT_BOARD_SIZE = document.getElementById('board-size');
+export const INPUT_COLOR = document.getElementById('new-color');
+export const INPUT_COLOR_CONTAINER = document.getElementById('box-new-color');
 export const NAV_OPITIONS = document.getElementById('nav-opitions');
+export const PIXEL_BOARD = document.getElementById('pixel-board');
+export const PALETTE_CONTAINER = document.getElementById('palette-container');
+export const WARNING_MESSAGE_PARAGRAPH = document.getElementById('warning-message');
